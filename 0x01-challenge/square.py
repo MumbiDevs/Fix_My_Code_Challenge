80ea0762a704fde673208b0a10bdb7ff4857f957
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-15 13:54:27 +0300

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 """Module that creates a square class"""
 
-
 class Square:
     """Creates Square class"""
 
-    def __init__(self, width=0, height=0):
-        """init method that initialises attributes"""
-        self.width = width
-        self.height = height
+    def __init__(self, side=0):
+        """init method that initializes attributes"""
+        self.side = side
 
     def area(self):
         """Calculate the area of the square"""
-        return self.width * self.height
+        return self.side ** 2
 
     def perimeter(self):
         """Calculate the perimeter of the square"""
-        return 2 * (self.width + self.height)
+        return 4 * self.side
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
-
+        return "Square with side length: {}".format(self.side)
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)
+    s = Square(side=12)
     print(s)
     print("Area:", s.area())
     print("Perimeter:", s.perimeter())
