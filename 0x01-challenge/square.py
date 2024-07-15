11e30688ec418dc7e89266638de1eefaecc77e5a
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-15 14:11:55 +0300

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-"""Module that creates a square class"""
+""" Defines Square class."""
 
-class Square:
-    """Creates Square class"""
 
-    def __init__(self, side=0):
-        """init method that initializes attributes"""
-        self.side = side
+class Square():
+    """ Square class."""
+    width = 0
+    height = 0
 
-    def area(self):
-        """Calculate the area of the square"""
-        return self.side ** 2
+    def __init__(self, *args, **kwargs):
+        """ Instantiation of class."""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-    def perimeter(self):
-        """Calculate the perimeter of the square"""
-        return 4 * self.side
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """ Perimeter of the square."""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "Square with side length: {}".format(self.side)
+        """ Printable representation of square object."""
+        return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
-    s = Square(side=12)
+    """ Creates Square object."""
+    s = Square(width=12, height=9)
     print(s)
-    print("Area:", s.area())
-    print("Perimeter:", s.perimeter())
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
