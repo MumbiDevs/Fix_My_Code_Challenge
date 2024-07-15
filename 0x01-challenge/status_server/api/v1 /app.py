465e9fc92aa03316a1758ca6a5c38f40ab28614b
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-15 14:02:33 +0300

    Update app.py

--- 0x01-challenge/status_server/api/v1 /app.py
+++ 0x01-challenge/status_server/api/v1 /app.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/v1/status', methods=['GET'])
+@app.route('status_server/api/v1/status', methods=['GET'])
 def get_status():
-    return jsonify({"status": "OK"})
+    return jsonify({"status": "OK"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
